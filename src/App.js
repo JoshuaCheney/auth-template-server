cbf16a372f6ccd5aa76d4d1179c104bc4ca392ff
Josh Cheney <joshcheney125@gmail.com>
2021-11-25 18:07:08 +0000

    sign up finished, working on sign in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
 function App() {
+  const [authenticatedUser, setAuthenticatedUser] = useState(null)
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  })
+  console.log("authenticateduser", authenticatedUser)
+
+  useEffect(() => {
+    const userFromLocalStorage = JSON.parse(localStorage.getItem("user"))
+    console.log("user from local storage", userFromLocalStorage)
+    setAuthenticatedUser(userFromLocalStorage)
+  }, [])
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json"
+      },
+      body: JSON.stringify({ ...user })
+    }
+
+    fetch("http://localhost:3030/signup", fetchOptions)
+      .then(res => res.json())
+      .then(data => {
+        const user = data.data
+        setAuthenticatedUser(user)
+        localStorage.setItem("user", JSON.stringify(user))
+      })
+  }
+
+  const handleSignIn = event => {
+    event.preventDefault()
+
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json"
+      },
+      body: JSON.stringify({ ...user })
+    }
+
+    fetch("http://localhost:3030/login", fetchOptions)
+      .then(res => res.json())
+      .then(data => {
+        const user = data.data
+
+        console.log("inside sign in fetch", user)
+        if (user) {
+          setAuthenticatedUser(user)
+          localStorage.setItem("user", JSON.stringify(user))
+        }
+      })
+  }
+
+
+  const handleChange = event => {
+    const name = event.target.name
+    const value = event.target.value
+    console.log("name", name)
+    console.log("value", value)
+
+    setUser({ ...user, [name]: value })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <form onSubmit={handleSubmit}>
+        <h2>Sign Up</h2>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" onChange={handleChange}></input>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" onChange={handleChange}></input>
+        <button type="submit">Sign Up</button>
+      </form>
+      <form onSubmit={handleSignIn}>
+        <h2>Log In</h2>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" onChange={handleChange}></input>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" onChange={handleChange}></input>
+        <button type="submit">Log in</button>
+      </form>
+      {authenticatedUser && <div>secrets</div>}
+    </main>
+  )
 }
 
-export default App;
+export default App
